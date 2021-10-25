accd10dfe314d382d0eca7f8d27cc59b6b64b1fe
Mike Hemberger <mike@thestizmedia.com>
2021-10-25 13:03:09 -0400

    Get it started

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Mai Exclusive theme.
+ *
+ * @package   BizBudding\MaiExclusive
+ * @link      https://bizbudding.com/
+ * @author    BizBudding
+ * @copyright Copyright © 2021 BizBudding
+ * @license   GPL-2.0-or-later
+ */
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+/**********************************
+ * Add your customizations below! *
+ **********************************/
